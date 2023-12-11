54913f62df8630bbfbbe91df3e9de325fbd54269
heidilea27 <152548065+heidilea27@users.noreply.github.com>
2023-12-11 15:24:00 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  // EVERYTHING GOES BELOW HERE
+ 
+  tellTheFuture.choices = ["Heidi", "Madelyn", "Ms. Hochwald", "Sarah Blaire"];
+
+  tellTheFuture.askQuestion = function(question) {
+   console.log(choices[0], choices[1], choices[3], choices[4])
+  }
+
+  
+
+  // EVERYTHING GOES ABOVE HERE
+});
